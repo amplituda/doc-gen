54a8676754bdc54c531903ed74ba880386e62564
Filip Weiss <me@fiws.net>
2015-06-15 12:06:32 +0200

    bin: fix relative paths not working. closes #7

--- bin/docgen.js
+++ bin/docgen.js
@@ -3,9 +3,16 @@
 
 var minimist = require('minimist');
 var docGenerator = require('../docgen');
+var resolve = require('path').resolve;
 
 // arguments
 var argv = minimist(process.argv.slice(2));
+
+// resolve paths
+// TODO: more pretty
+if (argv.entry) argv.entry = resolve(argv.entry);
+if (argv.output) argv.output = resolve(argv.output);
+if (argv.basePath) argv.basePath = resolve(argv.basePath);
 
 docGenerator.generateHtml({
   name: argv.name || 'VCL Documentation',
